3fbc6b17b19209b468ba456a3d2a8c3fa8a76c73
Tyler Hogsett <tyhogset@microsoft.com>
2020-10-26 08:55:32 -0700

    included filter for annotations

--- Add-WebsiteFilterToPortalSchema.ps1
+++ Add-WebsiteFilterToPortalSchema.ps1
@@ -23,15 +23,36 @@ $entityNodes | ForEach-Object {
             "<fetch>" +
                 "<entity name=`"$($entityNode.name)`">" +
                     "<attribute name=`"$($entityNode.primaryidfield)`" />" +
-                    "<link-entity name=`"adx_website`" from=`"adx_websiteid`" to=`"$($websiteLookupNode.name)`">" +
-                        "<filter>" +
-                            "<condition attribute=`"adx_name`" operator=`"eq`" value=`"$WebsiteName`" />" +
-                        "</filter>" +
+                    "<link-entity name=`"adx_webfile`" from=`"adx_webfileid`" to=`"regardingobjectid`">" +
+                        "<link-entity name=`"adx_website`" from=`"adx_websiteid`" to=`"adx_websiteid`">" +
+                            "<filter>" +
+                                "<condition attribute=`"adx_name`" operator=`"eq`" value=`"$WebsiteName`" />" +
+                            "</filter>" +
+                        "</link-entity>" +
                     "</link-entity>" +
                 "</entity>" +
             "</fetch>"
         $filterNode.InnerText = $fetchXmlQuery
     }
 }
+
+$annotationNode = $schemaXml.SelectSingleNode("/entities/entity[@name='annotation']")
+$filterNode = $annotationNode.SelectSingleNode("descendant::filter")
+if(-not $filterNode) {
+    $filterNode = $schemaXml.CreateElement("filter")
+    $annotationNode.AppendChild($filterNode) | Out-Null
+}
+$fetchXmlQuery = `
+    "<fetch>" +
+        "<entity name=`"annotation`">" +
+            "<attribute name=`"annotationid`" />" +
+            "<link-entity name=`"adx_website`" from=`"adx_websiteid`" to=`"$($websiteLookupNode.name)`">" +
+                "<filter>" +
+                    "<condition attribute=`"adx_name`" operator=`"eq`" value=`"$WebsiteName`" />" +
+                "</filter>" +
+            "</link-entity>" +
+        "</entity>" +
+    "</fetch>"
+$filterNode.InnerText = $fetchXmlQuery
 
 $schemaXml.Save($Path)
