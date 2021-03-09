948c6e1a0a3098b460b63540f12d2a30a3da90e3
Tyler Hogsett <tyhogset@microsoft.com>
2021-03-09 10:05:37 -0800

    set specific version for install-powershellmodules

--- setup/Install-PowerShellModules.ps1
+++ setup/Install-PowerShellModules.ps1
@@ -5,17 +5,27 @@ md $modulesFolder
 
 function Install-ModuleLocally {
   param(
-    [string]$Name
+    [string]$Name,
+    [string]$Version
   )
   
-  Find-Module `
-    -Name $Name -Repository 'PSGallery' `
-  | Save-Module -Path $modulesFolder
+  if($Version) {
+    Find-Module `
+      -Name $Name `
+      -Repository 'PSGallery' `
+      -RequiredVersion $Version `
+    | Save-Module -Path $modulesFolder
+  } else {
+    Find-Module `
+      -Name $Name `
+      -Repository 'PSGallery' `
+    | Save-Module -Path $modulesFolder
+  }
 }
 
 Install-ModuleLocally "Microsoft.Xrm.Tooling.CrmConnector.Powershell"
 Install-ModuleLocally "Microsoft.Xrm.Tooling.ConfigurationMigration"
 Install-ModuleLocally "Microsoft.Xrm.Data.Powershell"
-Install-ModuleLocally "Microsoft.PowerApps.Administration.PowerShell"
+Install-ModuleLocally "Microsoft.PowerApps.Administration.PowerShell" -Version "2.0.108"
 Install-ModuleLocally "Microsoft.PowerApps.PowerShell"
 Install-ModuleLocally "AzureAD"
